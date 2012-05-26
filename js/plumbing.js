4bf497965431fe5fa4addc147fb58da718dfddae
Christian Ortner <chris.ortner@gmail.com>
2012-05-26 19:07:15 +0200

    Displaying more information about individual downloads

--- js/plumbing.js
+++ js/plumbing.js
@@ -16,7 +16,12 @@ var resetGui = function () {
 };
 
 var showNewDownloadForm = function () {
-  // Make only viable options visible:
+  $('#new-download-dialog').dialog({
+    draggable: false,
+    modal: true,
+    resizable: false,
+    width: 400
+  });
 };
 
 var showSettings = function () {
@@ -69,6 +74,10 @@ var doRemove = function (evt) {
   Aria2.remove(gid);
 };
 
+var uploadDroppedFiles = function (evt) {
+  console.log(evt);
+};
+
 
 
 //------------------------------------------------------------------------------
@@ -82,6 +91,7 @@ var updateDownloadsAccordion = function (downloads) {
   // Insert HTML of new downloads and update existing ones:
   for (i in downloads) {
     var progress = downloads[i].completedLength / downloads[i].totalLength;
+    var name = downloads[i].files[0].path.split(downloads[i].dir)[1].substring(1);
 
     // Add new entry if download not present yet:
     var gidId = '#gid' + downloads[i].gid;
@@ -94,7 +104,9 @@ var updateDownloadsAccordion = function (downloads) {
       elem.appendTo('#downloads');
       accordionRebuildRequired = true;
 
+      // UI stuff:
       $(gidId + ' .progress-bar').progressbar();
+
       // Add handling functions for the controls:
       $(gidId + ' .resume-button').click(doResume);
       $(gidId + ' .pause-button').click(doPause);
@@ -105,12 +117,26 @@ var updateDownloadsAccordion = function (downloads) {
     $(gidId + ' .download-name').html(downloads[i].files[0].path);
     $(gidId + ' .download-rate').html(humanizeBytes(downloads[i].downloadSpeed, 2) + '/s');
     $(gidId + ' .upload-rate').html(humanizeBytes(downloads[i].uploadSpeed, 2) + '/s');
+    $(gidId + ' .download-progress-value').html((progress * 100).toFixed(1));
 
     $(gidId + ' .accordion-expansion').empty();
+    $(gidId + ' .accordion-expansion').append('<h3>Network:</h3><ul>' +
+        '<li><strong>Status:</strong> ' + downloads[i].status + '</li>' +
+        '<li><strong>Downloaded:</strong> ' + humanizeBytes(downloads[i].completedLength, 2) + '</li>' +
+        '<li><strong>Uploaded:</strong> ' + humanizeBytes(downloads[i].uploadLength, 2) + '</li>' +
+        '<li><strong>Total Size:</strong> ' + humanizeBytes(downloads[i].totalLength, 2) + '</li>' +
+        '<li><strong>Connections:</strong> ' + downloads[i].connections + '</li>' +
+      '</ul>');
     $(gidId + ' .accordion-expansion').append('<h3>Files:</h3><ul class="file-list"></ul>');
     var fileList = $(gidId + ' .file-list');
     for (j in downloads[i].files) {
-      $(gidId + ' .file-list').append('<li>' + downloads[i].files[j].path + '</li>');
+      var file = downloads[i].files[j];
+      var fileName = file.path.split(downloads[i].dir)[1].substring(1);
+      $(gidId + ' .file-list').append('<li><strong>' + fileName + ':</strong> ' +
+        'Downloaded ' + humanizeBytes(file.completedLength, 2) +
+        ' of ' + humanizeBytes(file.length, 2) +
+        ' (' + ((file.completedLength / file.length) * 100).toFixed(1) + ' % complete)' +
+        '</li>');
     }
 
 
@@ -178,4 +204,9 @@ $(document).ready(function () {
   $('#settings-button').click(showSettings);
   $('#info-button').click(showInfo);
   $('#shutdown-button').click(doShutdown);
+
+  $('#drop-zone').bind('drop', uploadDroppedFiles);
+  $('#drop-zone').bind('dragover', function (evt) {
+    console.log(evt);
+  });
 });
